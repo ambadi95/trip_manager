1b2e2c03d0fc2d81f62ce1c726a824d022208296
Ambadi Viswanath <ambadiavn.1995@gmail.com>
2025-01-23 10:22:12 +0530

    add expense - WIP

--- lib/features/dashboard/add.dart
+++ lib/features/dashboard/add.dart
@@ -1,17 +1,47 @@
-
-
-
 import 'package:flutter/material.dart';
+import 'package:trip_manager/core/%20colors/core_colors.dart';
+import 'package:trip_manager/core/components/space.dart';
 
 class Add extends StatelessWidget {
   const Add({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Add Expense'),
+    return Scaffold(
+        body: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
-    );
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Space(
+              height: 50,
+            ),
+            Container(
+              height: 60,
+              width: double.infinity,
+              color: primaryColor,
+              child: const Center(
+                child: Text('Current Month'),
+              ),
+            ),
+            const Space(
+              height: 10,
+            ),
+            Container(
+              height: 60,
+              width: double.infinity,
+              color: primaryColor,
+              child: const Center(
+                child: Text('Group'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
